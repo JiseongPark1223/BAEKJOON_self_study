225fa7679cbda14b36796d3e1f02164721ecb68e
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2023-01-14 23:37:09 +0900

    23.01.14

---
+++ study_more_BAEJOON/2439_print_star2/main1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    
+    scanf("%d", &num);
+    
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = num - 1 - i; j > 0; j--) //첫번째 for문에서는 공백의 개수를 조절한다.
+        {                                     //줄 수가 증가할수록 공백은 줄어들어야 하므로 i값의 증가를 이용한다. 
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++) //두번째 for문에서는 별의 개수를 조절한다.
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
