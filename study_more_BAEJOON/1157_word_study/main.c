943e1943260c99eeb76bc343cc9b46fd05fcf700
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2023-02-17 12:23:15 +0900

    23.02.17

--- study_more_BAEJOON/1157_word_study/main.c
+++ study_more_BAEJOON/1157_word_study/main.c
@@ -1,1 +1,52 @@
-dd
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char arr[1000001] = {0};
+    int max = 0, maxnum = 0, len, alpha[26], cnt = 0;
+    
+    scanf("%s", arr);
+    
+    len = strlen(arr);
+    
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] > 'Z') //소문자로 입력받은 문자를 대문자로 바꾼다.
+        {
+            arr[i] -= 32;
+        }
+        alpha[arr[i] - 65]++; //alpha라는 배열로 0 = a로 시작해서 25 = z로 한다.
+    }
+    
+    for (int i = 0; i < 26; i++)
+    {
+        if (alpha[i] > max)
+        {
+            max = alpha[i];
+            maxnum = i; //알파벳의 번째를 maxnum에 저장한다.
+        }
+    }
+    
+    for (int i = 0; i < 26; i++)
+    {
+        if (alpha[i] == max)
+        {
+            cnt++;
+        }
+    }
+    
+    if (cnt > 1) //최댓값이 겹치면 ?를 출력한다.
+    {
+        printf("?");
+    }
+    
+    else
+    {
+        printf("%c", 'A' + maxnum);
+    }
+    
+    return 0;
+}
+//알파벳을 아스키코드를 이용하여 배열로 표현하는 과정이 핵심이다.
