545bc1c00d83f17679124a5038af6a1b0ce1e0e6
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2023-03-28 18:49:55 +0900

    23.03.28

--- study_more_BAEJOON/10799_iron_stick/main.c
+++ study_more_BAEJOON/10799_iron_stick/main.c
@@ -4,12 +4,12 @@
 char stack[100001];
 int top;
 
-void push(char x)
+void push(char x) //stack push
 {
     stack[++top] = x;
 }
 
-void pop()
+void pop() //stack pop
 {
     top--;
 }
@@ -26,20 +26,20 @@ int main()
     
     for (int i = 0; i < len; i++)
     {
-        if (str[i] == '(')
+        if (str[i] == '(') //(은 쇠막대기가 시작하는 부분이므로 스택배열에 push한다.
         {
             push(str[i]);
         }
         else
         {
-            if (str[i - 1] == '(')
-            {
+            if (str[i - 1] == '(') //str[i - 1]이 '('이고 현재배열이 ')'이면 레이저란 뜻이다.
+            {                      //따라서 쌓인 쇠막대기를 잘린 갯수에 더하고 pop해준다.
                 cnt += top;
                 pop();
             }
             else
             {
-                cnt++;
+                cnt++; //레이저로 다 짤리고 남는 쇠막대기는 1개이기 때문에 cnt에 1을 더하고 pop한다.
                 pop();
             }
         }
