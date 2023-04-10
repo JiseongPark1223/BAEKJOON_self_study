913439fb8833df85d085ada3f159807990333b6e
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2023-04-10 22:13:07 +0900

    23.04.10

--- study_more_BAEJOON/17298_오큰수/main.c
+++ study_more_BAEJOON/17298_오큰수/main.c
@@ -25,8 +25,8 @@ int main()
         
     for (int i = t - 1; i >= 0; i--)
     {
-        while (top >= 0 && stack[top] <= arr[i])
-        {
+        while (top >= 0 && stack[top] <= arr[i]) //오른쪽부터 배열을 비교한다.
+        {                                        //확인할 배열의 오른쪽에 더 큰 배열이 있나 확인해본다.
             pop();
         }
         if (top == -1)
@@ -35,9 +35,9 @@ int main()
         }
         else
         {
-            res[i] = stack[top];
+            res[i] = stack[top]; //결과 배열에 오른쪽에서 가장 큰 수인 stack[top]을 저장한다.
         }
-        push(arr[i]);
+        push(arr[i]); //배열을 stack에 저장한다.
     }
     
     for (int i = 0; i < t; i++)
