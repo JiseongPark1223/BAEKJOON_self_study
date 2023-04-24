79c13b8a867582aa54f8f5f02fd520404eefb4c6
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2023-04-24 10:59:23 +0900

    23.04.24

---
+++ study_more_BAEJOON/17413_flip_word2/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+
+int top = -1;
+char stack[100003]; //fgets은 개행문자를 받고 경우에 따라 NULL을 추가하기 때문에 정해진 배열의 크기보다 2에서 3정도 크게 잡는다.
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+void pop()
+{
+    top--;
+}
+
+int main()
+{
+    int len;
+    char s[100003];
+    
+    fgets(s, 100003, stdin);
+    
+    len = strlen(s);
+    
+    if (s[len - 1] == '\n') //개행문자가 입력되기 때문에 그것을 NULL로 바꾸어준다.
+    {
+        s[len - 1] = 0;
+    }
+    
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] == '<')
+        {
+            if (top != -1) //<가 나오기 전에 스택에 있던 문자들은 뒤집어 출력한다.
+            {
+                while (top != -1)
+                {
+                    printf("%c", stack[top]);
+                    pop();
+                }
+            }
+            while (s[i] != '>') //괄호내 문자들은 순서가 뒤집히지 않으므로 그대로 출력한다.
+            {
+                printf("%c", s[i]);
+                i++;
+            }
+            printf("%c", s[i]);
+        }
+        else if (s[i] == ' ' || s[i] == '\0') //띄어쓰기가 나오거나 문자열 마지막에 오는 NULL을 만나면 뒤집어 출력한다.
+        {
+            while (top != -1)
+            {
+                printf("%c", stack[top]);
+                pop();
+            }
+            printf(" ");
+        }
+        else
+        {
+            push(s[i]);
+        }
+    }
+    return 0;
+}
