309751e9e7b4680fcce5744f052512aede61c0e2
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2023-05-22 11:43:52 +0900

    23.05.22

---
+++ study_more_BAEJOON/1966_printer_queue/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int t, cnt, p, max = 0, str[102], front = 0, res = 1;
+    
+    scanf("%d", &t);
+    
+    while (t--)
+    {
+        scanf("%d %d", &cnt, &p);
+        
+        front = 0, res = 1;
+        
+        for (int i = 0; i < cnt; i++)
+        {  
+            scanf("%d", &str[i]);
+        }
+        
+        while (1)
+        {
+            max = 0;
+            for (int i = 0; i < cnt; i++)
+            {
+                if (max < str[i])
+                {
+                    max = str[i];
+                }
+            }
+            
+            while (max != str[front])
+            {
+                front = (front + 1)%cnt;
+            }
+            
+            if (p == front)
+            {
+                break;
+            }
+            
+            str[front] = 0;
+            front = (front + 1)%cnt;
+            res++;
+        }
+        printf("%d\n", res);
+    }
+    
+    return 0;
+}
