ba8d6e2815a9535b2fdaad144cbaa291acbf44bd
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2023-06-04 14:23:58 +0900

    23.06.04

--- study_more_BAEJOON/1926_picture/main.c
+++ study_more_BAEJOON/1926_picture/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int map[502][502], vis[502][502] = {0}, dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+int map[502][502], vis[502][502] = {0}, dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1}; //그림, 방문 표시 배열, 상하좌우를 살필 배열을 선언한다.
 int n, m, front = 0, rear = 0, cnt = 0, width, max = 0;
 
 typedef struct q {
     int x, y;
-} q;
+} q; //x와 y 값 두개이므로 구조체를 선언하여 큐에 넣는다.
 
 q que[502 * 502];
 
@@ -14,7 +14,7 @@ void push(int x, int y)
     que[rear].x = x;
     que[rear].y = y;
     rear++;
-    vis[x][y] = 1;
+    vis[x][y] = 1; //큐에 넣을때 방문표시를 해준다.
 }
 
 void pop()
@@ -33,10 +33,10 @@ void bfs(int x, int y)
             nx = que[front].x + dx[i];
             ny = que[front].y + dy[i];
             
-            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-            if (map[nx][ny] != 1 || vis[nx][ny] == 1) continue;
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue; //그림크기에서 나간 범위는 포함하지 않는다.
+            if (map[nx][ny] != 1 || vis[nx][ny] == 1) continue;//그림에 표시되어 있지 않거나 방문한 적이 있는 경우 포함하지 않는다.
             
-            vis[nx][ny] = 1;
+            vis[nx][ny] = 1; //위 조건에 걸리지 않았다면 방문한적 없는 색칠된 그림이므로 방문표시를 하고 큐에 넣는다.
             push(nx, ny);
             width++;
         }
